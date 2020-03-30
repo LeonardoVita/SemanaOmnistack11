e51ffdde2992e4fa580c4dbd1e672b2c8d41cf4c
Leonardo Vita Milanez de Almeida <Leonardo.vita0606@gmail.com>
2020-03-29 21:12:00 -0300

    Testes e Validações da aplicação

---
+++ back-end/tests/integration/ong.spec.js
@@ -0,0 +1,33 @@
+const request = require('supertest')
+const app = require('../../src/app')
+const connection = require('../../src/database/connection')
+
+
+describe('ONG', () => {
+
+  beforeEach(async () => {
+    await connection.migrate.rollback()
+    await connection.migrate.latest()
+  })
+
+  afterAll(async () => {
+    await connection.destroy();
+  })
+
+  it('should be able to create a new ONG', async () => {
+    const response = await request(app)
+      .post('/ongs')
+      .send({
+        name: "Tecnologia e café",
+        email: "Tecoffe@org.com",
+        whatsapp: "552195587961",
+        city: "São Paulo",
+        uf: "SP"
+      })
+
+    expect(response.body).toHaveProperty('id')
+    expect(response.body.id).toHaveLength(8)
+  })
+})
+
+  // .set('Authorization', 'asd')
